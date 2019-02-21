ee925ffcf3f3ea6b9429102fec10f5f208482584
Olek <aleksander.wojtal@tieto.pl>
2019-02-21 18:28:25 +0100

    ok

--- makefiles.sh
+++ makefiles.sh
@@ -5,20 +5,18 @@ NAME=$2
 DIR=$3
 TEXT=$4
 
-while true; do
-	if [ -d $DIR ]; then
-		for i in $(seq 0 ${NUM}); do
-			touch ${DIR}${NAME}${i}
-			echo "$TEXT" > ${DIR}${NAME}${i}
-		done
-		break
-	else
-		echo "$DIR is not valid directory."
-		echo -n "Do you want to create directory $DIR? (Yes/no) "; read OPT
-		case $OPT in
-			Y|y|yes|YES|Yes ) mkdir $DIR; continue ;;
-			N|n|no|NO|No ) echo "Pick an existing directory."; break ;;
-			* ) mkdir $DIR; continue ;;
-		esac
-	fi
+if [ ! -d $DIR ]; then
+	echo "$DIR is not valid directory."
+	echo -n "Do you want to create directory $DIR? (Yes/no) "; read OPT
+	case $OPT in
+		Y|y|yes|YES|Yes ) mkdir $DIR ;;
+		N|n|no|NO|No ) echo "Pick an existing directory." ;;
+		* ) mkdir $DIR ;;
+	esac
+fi	
+
+for i in $(seq 0 ${NUM}); do
+	touch ${DIR}${NAME}${i}
+	echo "$TEXT" > ${DIR}${NAME}${i}
 done
+
